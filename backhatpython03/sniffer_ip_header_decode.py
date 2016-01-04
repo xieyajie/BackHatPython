a97f018c3055a5156cf40d5890723490b6af3c47
XieYajie <xyjxyf@163.com>
2016-01-04 22:28:54 +0800

    update decode ip header

--- backhatpython03/sniffer_ip_header_decode.py
+++ backhatpython03/sniffer_ip_header_decode.py
@@ -7,6 +7,9 @@ from ctypes import *
 
 host = "127.0.0.1"
 
+# Structures和Unions必须继承Structure和Union基础类，它们都在ctypes模块中定义，
+# 每一个子类必须定义个_fields_属性，_fields_是一个二维的tuples列表，包含着每个field的name及type，
+# 这field类型必须是一个ctypes类型，如c_int，或者任何其他的继承ctypes的类型，如Structure, Union, Array, 指针等
 class IP(Structure):
     _fields_ = [
         # 头长度, 4位
@@ -14,30 +17,39 @@ class IP(Structure):
         # 版本号, 4位. 目前采用的IP协议的版本号,一般的值为0100（IPv4），0110（IPv6）
         ("version", c_ubyte, 4),
         # 服务类型, 8位
-        ("tos", c_ubyte),
+        ("tos", c_ubyte, 8),
         # 包总长, 16位
-        ("len", c_ushort),
+        ("len", c_ushort, 16),
         # 标识符, 16位
-        ("id", c_ushort),
+        ("id", c_ushort, 16),
         # 标志, 3位
-        ("tag", c_ubyte),
+        # ("tag", c_ubyte, 3),
         # 片偏移, 13位
-        ("offset", c_ushort),
+        ("offset", c_ushort, 16),
         # 生存时间, 8位. 当IP包经过每一个沿途的路由器的时候，每个沿途的路由器会将IP包的TTL值减少1。如果TTL减少为0，则该IP包会被丢弃
-        ("ttl", c_ubyte),
+        ("ttl", c_ubyte, 8),
         # 协议, 8位. 1:ICMP 2:IGMP 6:TCP 17:UDP 88:IGRP 89:OSPF
-        ("protocol_num", c_ubyte),
+        ("protocol_num", c_ubyte, 8),
         # 头部校验, 16位
-        ("sum", c_ushort),
+        ("sum", c_ushort, 8),
+        # c_ulong is 4 bytes in i386 and 8 in amd64
+        ## 发送地址, 32位
+        #("src", c_ulong, 32),
+        ## 目标地址, 32位
+        #("dst", c_ulong, 32)
         # 发送地址, 32位
-        ("src", c_ulong),
+        ("src", c_uint32, 32),
         # 目标地址, 32位
-        ("dst", c_ulong)
+        ("dst", c_uint32, 32)
     ]
 
-    # def __new__(cls, socket_buffer=None):
-    #     # 将原始缓冲区中的数据填充到结构中
-    #     return cls.from_buffer_copy(socket_buffer)
+    # __new__() 静态方法. 实例化开始后,在__init__()之前调用. 没有重写,会自动调用父类
+    # __new__()会返回cls的实例，然后该类的__init__()方法作为构造方法会接收这个实例（即self）作为自己的第一个参数，然后依次传入__new__()方法中接收的位置参数和命名参数
+    # 如果__new__()没有返回cls（即当前类）的实例，那么当前类的__init__()方法是不会被调用的。
+    # 如果__new__()返回其他类（新式类或经典类均可）的实例，那么只会调用被返回的那个类的构造方法.
+    def __new__(cls, socket_buffer=None):
+        # 将原始缓冲区中的数据填充到结构中
+        return cls.from_buffer_copy(socket_buffer)
 
     def __init__(self, socket_buffer=None):
         self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
@@ -51,8 +63,8 @@ class IP(Structure):
         #  计算给定的格式(fmt)占用多少字节的内存
 
         # socket.inet_ntoa: 将32位形式的IP地址转换成字符串形式的IP地址,非线程安全
-        # self.src_address = socket.inet_ntoa(struct.pack("<L", self.src))
-        # self.dst_address = socket.inet_ntoa(struct.pack("<L", self.dst))
+        self.src_address = socket.inet_ntoa(struct.pack("@I", self.src))
+        self.dst_address = socket.inet_ntoa(struct.pack("@I", self.dst))
         # self.src_address = self.src
         # self.dst_address = self.dst
 
@@ -61,7 +73,7 @@ class IP(Structure):
         except:
             self.protocol = str(self.protocol_num)
 
-    def decodeBuffer(self, buffer=None):
+    def decode_buffer(self, buffer=None):
         if buffer:
             self.version = (int(ord(buffer[0])) & 0xF0) >> 4
             self.ih1 = (int(ord(buffer[0])) & 0x0F) << 2
@@ -75,8 +87,6 @@ class IP(Structure):
             self.sum = int(ord(buffer[10]) << 8)+int(ord(buffer[11]))
             self.src_address = "%d.%d.%d.%d" % (int(ord(buffer[12])), int(ord(buffer[13])), int(ord(buffer[14])), int(ord(buffer[15])))
             self.dst_address = "%d.%d.%d.%d" % (int(ord(buffer[16])), int(ord(buffer[17])), int(ord(buffer[18])), int(ord(buffer[19])))
-
-
 
 
 if os.name == "nt":
@@ -95,7 +105,10 @@ if os.name == "nt":
 try:
     while True:
         raw_buffer = sniffer.recvfrom(65565)[0]
-        ip_header = IP(raw_buffer[0:20])
+        # ip_header = IP(raw_buffer[:20]) works on x86 Ubuntu.
+        # ip_header = IP(raw_buffer[:32]) works on amd64 CentOS 6.6 Python 2.6.6
+        # ip_header = IP(raw_buffer) works in both.
+        ip_header = IP(raw_buffer)
 
         # ip_header = IP(raw_buffer)
         # ip_header.decodeBuffer(raw_buffer)
