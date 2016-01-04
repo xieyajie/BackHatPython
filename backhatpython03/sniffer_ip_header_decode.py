7570ad50878470e35d4f61232f87e9afbe1bbe21
xieyajie <xyjxyf@163.com>
2016-01-04 19:50:58 +0800

    update

--- backhatpython03/sniffer_ip_header_decode.py
+++ backhatpython03/sniffer_ip_header_decode.py
@@ -19,7 +19,9 @@ class IP(Structure):
         ("len", c_ushort),
         # 标识符, 16位
         ("id", c_ushort),
-        # 片偏移, 16位
+        # 标志, 3位
+        ("tag", c_ubyte),
+        # 片偏移, 13位
         ("offset", c_ushort),
         # 生存时间, 8位. 当IP包经过每一个沿途的路由器的时候，每个沿途的路由器会将IP包的TTL值减少1。如果TTL减少为0，则该IP包会被丢弃
         ("ttl", c_ubyte),
@@ -33,10 +35,13 @@ class IP(Structure):
         ("dst", c_ulong)
     ]
 
-    def __new__(cls, socket_buffer=None):
-        return cls.from_buffer_copy(socket_buffer)
+    # def __new__(cls, socket_buffer=None):
+    #     # 将原始缓冲区中的数据填充到结构中
+    #     return cls.from_buffer_copy(socket_buffer)
 
     def __init__(self, socket_buffer=None):
+        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
+
         # struct
         # pack(fmt, v1, v2, ...)
         # 按照给定的格式(fmt)，把数据封装成字符串(实际上是类似于c结构体的字节流)
@@ -44,28 +49,34 @@ class IP(Structure):
         # 按照给定的格式(fmt)解析字节流string，返回解析出来的tuple
         # calcsize(fmt)
         #  计算给定的格式(fmt)占用多少字节的内存
-        # iph = struct.unpack('!BBHHHBBH4s4s', socket_buffer)
-        # self.version = iph[0] >> 4
-        # self.ih1 = iph[0] * 0xF
-        # self.tos = hex(int(ord(socket_buffer[1])))
-        # self.len = self.ih1 * 4
-        # self.id = (int(ord(socket_buffer[4]) >> 8)) + (int(ord(socket_buffer[5])))
-        # self.ttl = iph[5]
-        # self.protocol_num = iph[6]
-        # self.src = socket.inet_ntoa(struct.pack(iph[8]))
-        # self.dst = socket.inet_ntoa(struct.pack(iph[9]))
-
-        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
 
         # socket.inet_ntoa: 将32位形式的IP地址转换成字符串形式的IP地址,非线程安全
-        # struct.pack: 转换成字节流
-        # self.src_address = socket.inet_ntoa(struct.pack("L", self.src))
-        # self.dst_address = socket.inet_ntoa(struct.pack("L", self.dst))
+        # self.src_address = socket.inet_ntoa(struct.pack("<L", self.src))
+        # self.dst_address = socket.inet_ntoa(struct.pack("<L", self.dst))
+        # self.src_address = self.src
+        # self.dst_address = self.dst
 
         try:
             self.protocol = self.protocol_map[self.protocol_num]
         except:
             self.protocol = str(self.protocol_num)
+
+    def decodeBuffer(self, buffer=None):
+        if buffer:
+            self.version = (int(ord(buffer[0])) & 0xF0) >> 4
+            self.ih1 = (int(ord(buffer[0])) & 0x0F) << 2
+            # self.tos = hex(int(ord(socket_buffer[1])))
+            self.len = (int(ord(buffer[2]) << 8))+(int(ord(buffer[3])))
+            self.id = (int(ord(buffer[4]) >> 8)) + (int(ord(buffer[5])))
+            self.tag = int(ord(buffer[6]) & 0xE0) >> 5
+            self.offset = int(ord(buffer[6]) & 0x1F) << 8 + int(ord(buffer[7]))
+            self.ttl = int(ord(buffer[8]))
+            self.protocol_num = int(ord(buffer[9]))
+            self.sum = int(ord(buffer[10]) << 8)+int(ord(buffer[11]))
+            self.src_address = "%d.%d.%d.%d" % (int(ord(buffer[12])), int(ord(buffer[13])), int(ord(buffer[14])), int(ord(buffer[15])))
+            self.dst_address = "%d.%d.%d.%d" % (int(ord(buffer[16])), int(ord(buffer[17])), int(ord(buffer[18])), int(ord(buffer[19])))
+
+
 
 
 if os.name == "nt":
@@ -84,8 +95,11 @@ if os.name == "nt":
 try:
     while True:
         raw_buffer = sniffer.recvfrom(65565)[0]
-        ip_header = IP(raw_buffer)
-        print("Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src, ip_header.dst))
+        ip_header = IP(raw_buffer[0:20])
+
+        # ip_header = IP(raw_buffer)
+        # ip_header.decodeBuffer(raw_buffer)
+        print("Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
 
 except KeyboardInterrupt:
     if os.name == "nt":
